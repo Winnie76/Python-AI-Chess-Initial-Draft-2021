a923976873ac22fca6798ca365934219c3e8d53a
Winnie76 <1438290294@qq.com>
2021-03-22 11:00:51 +1100

    update

--- search/assign1 (1).py
+++ search/assign1 (1).py
@@ -1,40 +1,46 @@
 
-def func_sort_distance(goal_dictionary): #match a lower token for each upper token with shortest distance
-    sorted_goal_dict={}
+# match a lower token for each upper token with shortest distance
+def func_sort_distance(goal_dictionary):
+    sorted_goal_dict = {}
     for key in goal_dictionary:
-        min=goal_dictionary[key][0][3] #minimum distance
-        min_i=0
+        min = goal_dictionary[key][0][3]  # minimum distance
+        min_i = 0
         for i in range(len(goal_dictionary[key])):
-            if goal_dictionary[key][i][3]<min:
-                min=goal_dictionary[key][i][3]
-                min_i=i
+            if goal_dictionary[key][i][3] < min:
+                min = goal_dictionary[key][i][3]
+                min_i = i
             else:
                 pass
-        target={key:goal_dictionary[key][min_i]}
+        target = {key: goal_dictionary[key][min_i]}
         sorted_goal_dict.update(target)
     return sorted_goal_dict
 
-print(func_sort_distance(goal_dictionary))  
-token=('S', -3, 1)
 
-def six_hex_surrond(token): #find the six surronding hex for a given hex
-    if len(token)==3:
-        token=token[1:3]
-    action=[[-1,0],[0,-1],[1,-1],[1,0],[0,1],[-1,1]] #six direction list in clockwise order
-    six_hex=[]
+print(func_sort_distance(goal_dictionary))
+token = ('S', -3, 1)
+
+
+def six_hex_surrond(token):  # find the six surronding hex for a given hex
+    if len(token) == 3:
+        token = token[1:3]
+    action = [[-1, 0], [0, -1], [1, -1], [1, 0], [0, 1],
+              [-1, 1]]  # six direction list in clockwise order
+    six_hex = []
     for item in action:
-        x=item[0]+token[0]
-        y=item[1]+token[1]
-        loc=(x,y) #coordinate of the hex
+        x = item[0]+token[0]
+        y = item[1]+token[1]
+        loc = (x, y)  # coordinate of the hex
         six_hex.append(loc)
     return six_hex
 
-def if_ol_append(state,item,token,ol): #check if the item should be added to open list
-    if item not in ol: #avoid double record
-        if item in state: 
-            #append if not block or undefeatable lower token
-            if not ((state[item]=='') | (if_defeat(token,item)==0)):
+
+def if_ol_append(state, item, token, ol):  # check if the item should be added to open list
+    if item not in ol:  # to avoid double record
+        if item in state:
+            # append if not block or undefeatable lower token
+            if not ((state[item] == 'Block') | (if_defeat(token, item) == 0)):
                 ol.append(item)
+        # 这里的else是 如果 item不在state里吗？但如果不在， 那为什么存在？
         else:
             ol.append(item)
     else:
@@ -42,62 +48,66 @@ def if_ol_append(state,item,token,ol): #check if the item should be added to ope
     return ol
 
 
-def open_list(state, token,target): #record all the possible new location
-    #token means upper token
-    ol=[]
-    ol_with_cost=[]
-    layer1=six_hex_surrond(token) #six hex connected to the token
-    
-    #slide
+def open_list(state, token, target):  # record all the possible new location
+    # token means upper token
+    ol = []
+    ol_with_cost = []
+    layer1 = six_hex_surrond(token)  # six hex connected to the token
+
+    # slide
     for item in layer1:
-        ol=if_ol_append(state,item,token,ol)
-            
-    #swing
+        ol = if_ol_append(state, item, token, ol)
+
+    # swing
     for i in range(6):
-        item=layer1[i]
+        item = layer1[i]
         if (item in state):
-            if (state[item].isupper()): #have upper token --> can swing
-                layer2=six_hex_surrond(item) #six hex connected to the upper token for swing
-                for j in range(i-1,i+2,1): #three hex opposite side
-                    if (j==6): #the hex next to no.5 in the clockwise list is no.0 and no.4
-                        ol=if_ol_append(state,layer2[0],token,ol)
+            if (state[item].isupper()):  # have upper token --> can swing
+                # six hex connected to the upper token for swing
+                layer2 = six_hex_surrond(item)
+                for j in range(i-1, i+2, 1):  # three hex opposite side
+                    if (j == 6):  # the hex next to no.5 in the clockwise list is no.0 and no.4
+                        ol = if_ol_append(state, layer2[0], token, ol)
                     else:
-                        ol=if_ol_append(state,layer2[j],token,ol)
+                        ol = if_ol_append(state, layer2[j], token, ol)
     for item in ol:
-        item=list(item)
-        cost=func_upper_lower_distance(item, target)
+        item = list(item)
+        cost = func_upper_lower_distance(item, target)
         item.append(cost)
-        ol_with_cost.append(item)         
+        ol_with_cost.append(item)
     return ol_with_cost
 
+
 def update_open_close(key, open_close_dict, sorted_goal_dict):
-    
-    #renew open list, and put movement into close dict
-    target=sorted_goal_dict[key][0]
-    ol=open_list(state, key, target)
-    cl=open_close_dict[key][1]
-    min_dis=ol[0][2]
-    min_i=0
-    for i in range(len(ol)): #find the movement with least distance to target
-        if ol[i][2]<min_dis:
-            min_dis=ol[i][2]
-            min_i=i
-    cl.append(ol[min_i][0:2]) #renew close list
-    #state=func_update_state(state) #renew state
-    ol=open_list(state, key, target) #renew open list
-    open_close_dict[key]=[ol,cl] #renew open_close_list
+
+    # renew open list, and put movement into close dict
+    target = sorted_goal_dict[key][0]
+    ol = open_list(state, key, target)
+    cl = open_close_dict[key][1]
+    min_dis = ol[0][2]
+    min_i = 0
+    for i in range(len(ol)):  # find the movement with least distance to target
+        if ol[i][2] < min_dis:
+            min_dis = ol[i][2]
+            min_i = i
+    cl.append(ol[min_i][0:2])  # renew close list
+    # state=func_update_state(state) #renew state
+    ol = open_list(state, key, target)  # renew open list
+    open_close_dict[key] = [ol, cl]  # renew open_close_list
     return open_close_dict
 
-def open_close_dict_build(state, upper, sorted_goal_dictionary, close_list): #build initial open_close_dict
-    open_close_dict={}
+
+# build initial open_close_dict
+def open_close_dict_build(state, upper, sorted_goal_dictionary, close_list):
+    open_close_dict = {}
     for u in upper:
-        u=tuple(u)
-        target=sorted_goal_dictionary[u][0]
-        ol=open_list(state, token, target)
-        mid={u:[ol,close_list[u]]}
+        u = tuple(u)
+        target = sorted_goal_dictionary[u][0]
+        ol = open_list(state, token, target)
+        mid = {u: [ol, close_list[u]]}
         open_close_dict.update(mid)
     return open_close_dict
 
-#layer1[i]是item
-#layer记录顺序按顺时针
-#要记是在layer1 可以用来swing的u2是顺时针记录的list的第几个 (判断在u1的哪个位置）
+# layer1[i]是item
+# layer记录顺序按顺时针
+# 要记是在layer1 可以用来swing的u2是顺时针记录的list的第几个 (判断在u1的哪个位置）
--- search/main.py
+++ search/main.py
@@ -121,6 +121,37 @@ def main():
     #             open_close_dict = func_update_open(
     #                 upper_tuple, open_close_dict, sorted_goal_dict, state)
 
+    # # loop through all upper tokens again and print next move for all upper tokens
+    # # why use 2 loops seperately : if one upper token move directly, it's may not be the best move for all upper tokens cuz another upper token needs to change route??
+    # # maybe can find a way to put into one loop. but i will go with this method first
+    # for upper_tuple in all_uppers_list:
+    #     # upper token already reached all goals
+    #     if upper_tuple not in sorted_goal_dict.keys():
+    #         # NEED func_update_state HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!!!!!!!!!!!!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #         # func_update_state updates the state
+    #         # it can check if current upper token reached goal or not
+    #         # if reached goal already, then it will make a best random movement for upper token , update state and print the movement
+    #         # if not reached goal, then based on open_close_list movement, it will update the state according to that and print the movement
+    #         # so the function reads which upper token it is from argument, read the current state, read the open_close_dict to know which movement to update and then update the state
+    #         state = func_update_state(upper_tuple, state, open_close_dict)
+    #     # upper token still have goal to reach
+    #     else:
+    #         # NEED func_update_close HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!!!!!!!!!!!!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #         # func_update_close only updates the close list (which is the action/movement of that upper token) in open_close_dict
+    #         # it read upper_tuple from argument to know which upper token to update
+    #         # it reads open-close_list and make a decision based on all possible moves and current state of board
+    #         open_close_dict = func_update_close( upper_tuple, open_close_list, state)
+    #         # based on open_close_list movement, it will update the state according to that and print the movement
+    #         state = func_update_state(upper_tuple, state, open_close_dict)
+
+    #         # NEED func_check_goal_reached HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!!!!!!!!!!!!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #         # check if upper token reached goal or not
+    #         # if upper token reached its goal, delete that value in sorted_goal_dict, and delete that upper token (key&value) in open_close_list
+    #         # if upper token reached all goals, delete that key-value pair in sorted_goal_dict, and delete that upper token (key&value) in open_close_list
+    #         if func_check_goal_reached(upper_tuple, sorted_goal_dict, open_close_dict) == 1:
+    #             open_close_dict.remove(upper_tuple)
+    #             # NEED func_remove_value_or_both HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!!!!!!!!!!!!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #             sorted_goal_dict = func_remove_value_or_both(upper_tuple, sorted_goal_dict, open_close_dict)
 
 #--------------------------------------------functions needed-------------------------------------------------------#
 # def func_update_open(upper_tuple, open_close_dict, sorted_goal_dict, state):
